6aa56deff24e55bf32d8cd964f4b56ff732d483b
robinhoets <robertets88@gmail.com>
2021-01-31 21:50:04 -0500

    bootstrap scss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import './App.css';
+
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
+// import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <Router>
-      <Link to="/">Home</Link>
-    </Router>
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-primary">Search</Button>
+      </Form>
+    </Navbar>
   );
 }
 
 export default App;
+
+// <Router>
+//
+//   <div className="row">
+//     <div className="App-header">
+//       <Link to="/">Home</Link>
+//       <Link to="/">Articles</Link>
+//       <Link to="/">About</Link>
+//     </div>
+//   </div>
+// </Router>
