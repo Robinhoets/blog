84038d05d148ac43cfe2c29d3f75f52f151378e6
robinhoets <robertets88@gmail.com>
2021-02-01 21:41:59 -0500

    landing page image

--- src/App.js
+++ src/App.js
@@ -12,23 +12,30 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import FormControl from 'react-bootstrap/FormControl'
-// import './App.css';
+import './App.css';
 import './App.scss';
+
+import Field from './images/LivField.jpeg'
 
 function App() {
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">CSLoop</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Router>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Articles</Nav.Link>
+          <Nav.Link href="#pricing">Statistics</Nav.Link>
+        </Router>
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-primary">Search</Button>
+        <Button variant="outline-info">Search</Button>
       </Form>
     </Navbar>
+    <img className="Image" src={Field} alt="Italian Trulli" />
+    </>
   );
 }
 
